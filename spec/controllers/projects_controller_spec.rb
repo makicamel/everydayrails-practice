84f0ce9a940f95d744ac8ca3a94d76d7e3af0e80
makicamel <unright@gmail.com>
2019-11-29 23:52:49 +0900

    Add projects#update spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -77,4 +77,49 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+  describe "#update" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+      it "updates a project" do
+        project_params = FactoryBot.attributes_for(:project, name: "New project name")
+        sign_in @user
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(@project.reload.name).to eq "New project name"
+      end
+    end
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user, email: "test2@example.com")
+        @project = FactoryBot.create(:project, owner: other_user, name: "Same old name")
+      end
+      it "does not update the project" do
+        sign_in @user
+        patch :update, params: { id: @project.id, project: { name: "New project name" } }
+        expect(@project.reload.name).to eq "Same old name"
+      end
+      it "redirects to the dashboard" do
+        sign_in @user
+        patch :update, params: { id: @project.id, project: { name: "New project name" } }
+        expect(response).to redirect_to root_path
+      end
+    end
+    context "as a guest" do
+      before do
+        user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: user)
+      end
+      it "returns a 302 status" do
+        patch :update, params: { id: @project.id, project: { name: "New project name" } }
+        expect(response).to have_http_status "302"
+      end
+      it "redirects to sign-in page" do
+        patch :update, params: { id: @project.id, project: { name: "New project name" } }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
