07b152be65e8097b53823f0193fcd06b49a6a43f
makicamel <unright@gmail.com>
2019-11-29 23:23:08 +0900

    Add projects#create spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -53,4 +53,28 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+  describe "#create" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      it "adds a project" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        expect { post :create, params: { project: project_params } }.to change(@user.projects, :count).by(1)
+      end
+    end
+    context "as an unauthorized user" do
+      it "returns a 302 status" do
+        project_params = FactoryBot.attributes_for(:project)
+        post :create, params: { project: project_params }
+        expect(response).to have_http_status "302"
+      end
+      it "redirects to the sign-in page" do
+        project_params = FactoryBot.attributes_for(:project)
+        post :create, params: { project: project_params }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
