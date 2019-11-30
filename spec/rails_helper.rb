2d092f7a5270ec33b493d64368f774d827ce0f3b
makicamel <unright@gmail.com>
2019-11-30 10:11:32 +0900

    Omit FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,6 +56,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include FactoryBot::Syntax::Methods
+
   # Use Devise helpers in tests
   config.include Devise::Test::ControllerHelpers, type: :controller
 end
